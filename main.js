cfba520b516853bfd48004b85f5530c7cbc9e9aa
Kyle Richardson <s.k.richardson7@gmail.com>
2015-05-25 16:46:41 -0400

    Able to target and display correct data using handle bars. Will likely need to start over SCSS.

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 (function(){
   'use strict';
 
-
+  var ulElement = document.querySelector('.product-grid');
 
 function fetchJSONP(url, callback) {
     var callbackName = 'jsonp_callback_' + Math.round(100000 * Math.random());
@@ -18,12 +18,22 @@ function fetchJSONP(url, callback) {
 }
 
 
-function logData(response) {
+function data(response) {
  console.log(response.results);
+  var results = response.results;
+  displayProducts(results);
  }
 
+
 var url = "https://api.etsy.com/v2/listings/active.js?api_key=19vjkb65j5uuobqlp0889tzd&keywords=caferacer&includes=Images,Shop";
-fetchJSONP(url, logData);
+fetchJSONP(url, data);
 
-
+function displayProducts(products) {
+  var source   = document.querySelector("#product-template").innerHTML;
+  var template = Handlebars.compile(source);
+  products.forEach(function(product){
+    var output = template(product);
+    ulElement.insertAdjacentHTML('beforeend', output);
+  });
+}
 })();
